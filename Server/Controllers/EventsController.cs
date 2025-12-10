3b1665113737478f8e2238d6509089eeb3f1ef9b
Filza Saleem <filza.saleem05@gmail.com>
2025-12-10 13:29:40 +0100

    added curd in controller

--- Server/Controllers/EventsController.cs
+++ Server/Controllers/EventsController.cs
@@ -5,6 +5,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Server.DTOs;
 using Server.Services;
 
 namespace Server.Controllers
@@ -28,6 +29,42 @@ namespace Server.Controllers
             if (clerkUserId == null)
                 return Unauthorized();
             return Ok(await _eventService.GetEventsAsync(clerkUserId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEvent([FromBody] EventCreateDto request)
+        {
+            var clerkUserId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (clerkUserId == null)
+                return Unauthorized();
+            var createEvent = await _eventService.CreateEventAsync(clerkUserId, request);
+            return CreatedAtAction(nameof(GetEvents), new { id = createEvent.Id }, createEvent);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEvent(Guid id)
+        {
+            var clerkUserId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (clerkUserId == null)
+                return Unauthorized();
+            var deleted = await _eventService.DeleteEventAsync(clerkUserId, id);
+            if (!deleted)
+                return NotFound();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEvent(Guid id, [FromBody] EventCreateDto request)
+        {
+            var clerkUserId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (clerkUserId == null)
+                return Unauthorized();
+            var updatedEvent = await _eventService.UpdateEventAsync(clerkUserId, id, request);
+            if (updatedEvent == null)
+                return NotFound();
+
+            return Ok(updatedEvent);
         }
     }
 }
